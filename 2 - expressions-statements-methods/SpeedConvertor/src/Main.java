68b2aee54299bbd030740303665a56fd08bfb3e8
Mubarek Seid <mubareksd@gmail.com>
2022-04-15 15:05:07 +0300

    56. Coding Exercises Example Part 2

--- 2 - expressions-statements-methods/SpeedConvertor/src/Main.java
+++ 2 - expressions-statements-methods/SpeedConvertor/src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+
+    public static void main(String[] args) {
+        long miles = SpeedConvertor.twoMilesPerHour(10.5);
+        System.out.println("Miles = " + miles);
+
+        SpeedConvertor.printConversion(10.5);
+    }
 }
--- 2 - expressions-statements-methods/SpeedConvertor/src/SpeedConvertor.java
+++ 2 - expressions-statements-methods/SpeedConvertor/src/SpeedConvertor.java
@@ -7,4 +7,13 @@ public class SpeedConvertor {
     return Math.round(kilometersPerHour / 1.609);
 
     }
+
+    public static void printConversion (double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long milesPerHour = twoMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+        }
+    }
 }
