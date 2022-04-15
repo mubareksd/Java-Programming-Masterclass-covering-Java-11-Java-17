1d0b0e9bf65c0ff3a44bc9cae4a67dc999b860a8
Mubarek Seid <mubareksd@gmail.com>
2022-04-15 15:26:07 +0300

    Coding Exercise 10: Equality Printer

---
+++ 2 - expressions-statements-methods/EqualityPrinter/src/io/github/ds4647318/Main.java
@@ -0,0 +1,12 @@
+package io.github.ds4647318;
+
+public class Main {
+
+    public static void main(String[] args) {
+        EqualityPrinter.printEqual(1,1,1);
+        EqualityPrinter.printEqual(1,1,2);
+        EqualityPrinter.printEqual(-1,-1,-1);
+        EqualityPrinter.printEqual(1,2,3);
+
+    }
+}
