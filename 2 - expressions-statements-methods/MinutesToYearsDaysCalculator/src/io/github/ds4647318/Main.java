7131ae21f1cdea44fa0ed5f437e9171b3090bc1d
Mubarek Seid <mubareksd@gmail.com>
2022-04-15 15:25:15 +0300

    Coding Exercise 9: Minutes To Years Days Calculator

---
+++ 2 - expressions-statements-methods/MinutesToYearsDaysCalculator/src/io/github/ds4647318/Main.java
@@ -0,0 +1,10 @@
+package io.github.ds4647318;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MinutesToYearsDaysCalculator.printYearsAndDays(-525600);
+        MinutesToYearsDaysCalculator.printYearsAndDays(1051200);
+        MinutesToYearsDaysCalculator.printYearsAndDays(561600);
+    }
+}
