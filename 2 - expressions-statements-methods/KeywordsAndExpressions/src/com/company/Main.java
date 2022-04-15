2c6ed57b6b68a966ce727de3b094c3ba314c9711
Mubarek Seid <mubareksd@gmail.com>
2022-04-15 14:21:47 +0300

    44. Keywords And Expressions

---
+++ 2 - expressions-statements-methods/KeywordsAndExpressions/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package io.github.ds4647318;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // a mile is equal to 1.609344 kilometres
+        double kilometres = (100 * 1.609344);
+        int highScore = 50;
+
+        if (highScore == 50) {
+            System.out.println("This is an expression");
+        }
+
+        // In the following code that I will type billow, write down that parts of the code
+        // are expressions
+        int score = 100;
+        if (score > 99) {
+            System.out.println("You got the high score!");
+            score = 0;
+        }
+
+    }
+}
