7828ca668e92006c77e7ee3b82ae45544f53e3e6
Mubarek Seid <mubareksd@gmail.com>
2022-04-15 13:35:30 +0300

    26. byte, short, long and width

--- 1- variables-datatypes-operators/ByteShortIntLong/src/io/github/ds4647318/Main.java
+++ 1- variables-datatypes-operators/ByteShortIntLong/src/io/github/ds4647318/Main.java
@@ -11,7 +11,30 @@ public class Main {
         System.out.println("Integer Max Value = " + myMaxIntValue);
         System.out.println("Busted Max Value = " + (myMaxIntValue + 1));
         System.out.println("Busted Min Value = " + (myMinIntValue - 1));
-        
+
         int myMaxIntTest = 2_147_483_647;
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Maximum Value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Short Maximum Value = " + myMaxShortValue);
+
+        long myLongValue = 100L;
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Maximum Value = " + myMaxLongValue);
+        long bigLongLiteralValue = 2_147_483_647L;
+        System.out.println(bigLongLiteralValue);
+
+        short bigShortLiteralValue = 32767;
     }
 }
