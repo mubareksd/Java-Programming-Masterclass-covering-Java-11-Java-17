c7998a25f7f0ec712007cbbee67321044591b1f0
Mubarek Seid <mubareksd@gmail.com>
2022-04-15 14:14:24 +0300

    40. Operator Precedence and Operator Challenge

--- 1- variables-datatypes-operators/Operators/src/io/github/ds4647318/Main.java
+++ 1- variables-datatypes-operators/Operators/src/io/github/ds4647318/Main.java
@@ -81,5 +81,19 @@ public class Main {
         if (wasCar) {
             System.out.println("wasCar is true");
         }
+
+        double myFirstValue = 20.00d;
+        double mySecondValue = 80.00d;
+        double myValuesTotal = (myFirstValue + mySecondValue) * 100.00d;
+        System.out.println("myValuesTotal = " + myValuesTotal);
+        double theRemainder = myValuesTotal % 40.00d;
+        System.out.println("theRemainder = " + theRemainder);
+        boolean isNoRemainder = (theRemainder == 0) ? true : false;
+        System.out.println("isNoRemainder = " + isNoRemainder);
+
+        if (!isNoRemainder) {
+            System.out.println("Got some remainder");
+        }
+
     }
 }
