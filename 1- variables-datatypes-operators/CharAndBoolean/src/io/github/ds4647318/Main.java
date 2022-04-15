fe69796e576b96f57374448f57f8788278dc68be
Mubarek Seid <mubareksd@gmail.com>
2022-04-15 13:52:26 +0300

    31. The char and boolean Primitive Data Types

---
+++ 1- variables-datatypes-operators/CharAndBoolean/src/io/github/ds4647318/Main.java
@@ -0,0 +1,17 @@
+package io.github.ds4647318;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char myChar = 'D';
+        char myUnicode = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicode);
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+        boolean myTrueBoole = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+    }
+}
