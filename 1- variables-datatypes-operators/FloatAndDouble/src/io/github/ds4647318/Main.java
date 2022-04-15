2603ec88cc1392155ab24bc723fb353cfaa5635b
Mubarek Seid <mubareksd@gmail.com>
2022-04-15 13:45:19 +0300

    29. float and double Primitive Types

---
+++ 1- variables-datatypes-operators/FloatAndDouble/src/io/github/ds4647318/Main.java
@@ -0,0 +1,24 @@
+package io.github.ds4647318;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float maximum value = " + myMaxFloatValue);
+        System.out.println();
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double minimum value = " + myMinDoubleValue);
+        System.out.println("Double maximum value = " + myMaxDoubleValue);
+        System.out.println();
+
+        int myIntValue = 5;
+        float myFloatValue = 5.25f;
+        double myDoubleValue = 5.25d;
+
+    }
+}
