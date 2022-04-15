610bf759421b1548ad3814ce903d3446e7b5170b
Mubarek Seid <mubareksd@gmail.com>
2022-04-15 14:50:58 +0300

    50. Method Challenge - Final Code Changes

--- 2 - expressions-statements-methods/Methods/src/io/github/ds4647318/Main.java
+++ 2 - expressions-statements-methods/Methods/src/io/github/ds4647318/Main.java
@@ -60,16 +60,26 @@ public class Main {
     }
 
     public static int calculateHighScorePosition(int playerScore) {
-       if (playerScore >= 1000) {
-           return 1;
-       } else if (playerScore >= 500) {
-           return 2;
-       } else if (playerScore >= 100) {
-           return 3;
-       }
+//        if (playerScore >= 1000) {
+//            return 1;
+//        } else if (playerScore >= 500) {
+//            return 2;
+//        } else if (playerScore >= 100) {
+//            return 3;
+//        }
+//
+//        return 4;
 
-       return 4;
+        int position = 4; // asuuming position 4 will be returned
 
+        if (playerScore >=1000) {
+            position = 1;
+        } else if (playerScore >= 500) {
+            position = 2;
+        } else if (playerScore >= 100) {
+            position = 3;
+        }
+        return position;
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
