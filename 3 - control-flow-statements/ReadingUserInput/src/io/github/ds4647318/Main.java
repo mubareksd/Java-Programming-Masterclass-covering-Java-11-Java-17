40bb0ceaa47032d88bc2db938b326f78e5f7764c
Mubarek Seid <mubareksd@gmail.com>
2022-04-16 13:05:24 +0300

    72. Reading User Input

---
+++ 3 - control-flow-statements/ReadingUserInput/src/io/github/ds4647318/Main.java
@@ -0,0 +1,26 @@
+package io.github.ds4647318;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your year of birth: ");
+        int yearOfBirth = scanner.nextInt();
+
+        scanner.nextLine(); // handle next line charcter (enter key)
+
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+
+        int age = 2022 - yearOfBirth;
+
+        System.out.println("Your name is " + name + " and you are " + age + " years old.");
+
+        scanner.close();
+
+    }
+}
