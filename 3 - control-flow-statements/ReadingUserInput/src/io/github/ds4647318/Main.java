fc0cab4e051352f3afe057a56e646960d887cc92
Mubarek Seid <mubareksd@gmail.com>
2022-04-16 13:18:15 +0300

    73. Problems and Solutions

--- 3 - control-flow-statements/ReadingUserInput/src/io/github/ds4647318/Main.java
+++ 3 - control-flow-statements/ReadingUserInput/src/io/github/ds4647318/Main.java
@@ -9,16 +9,29 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter your year of birth: ");
-        int yearOfBirth = scanner.nextInt();
 
-        scanner.nextLine(); // handle next line charcter (enter key)
+        boolean hasNextInt = scanner.hasNextInt();
 
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
+        if (hasNextInt) {
 
-        int age = 2022 - yearOfBirth;
+            int yearOfBirth = scanner.nextInt();
 
-        System.out.println("Your name is " + name + " and you are " + age + " years old.");
+            scanner.nextLine(); // handle next line charcter (enter key)
+
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
+
+            int age = 2022 - yearOfBirth;
+
+            if (age > 0 && age <= 99) {
+                System.out.println("Your name is " + name + " and you are " + age + " years old.");
+            } else {
+                System.out.println("Invalid year of birth");
+            }
+
+        } else {
+            System.out.println("Unable to parse year of birth");
+        }
 
         scanner.close();
 
