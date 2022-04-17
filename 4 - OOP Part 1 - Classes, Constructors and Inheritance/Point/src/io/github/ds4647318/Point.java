7ce9302f2d58cdf074c504efe1f4d648d5d7940e
Mubarek Seid <mubareksd@gmail.com>
2022-04-17 19:44:36 +0300

    Coding Exercise 32: Point

---
+++ 4 - OOP Part 1 - Classes, Constructors and Inheritance/Point/src/io/github/ds4647318/Point.java
@@ -0,0 +1,44 @@
+package io.github.ds4647318;
+
+public class Point {
+
+    private int x;
+    private int y;
+
+    public Point() {
+
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distance () {
+        return distance(0, 0);
+    }
+
+    public double distance (int x, int y) {
+        return Math.sqrt((this.x - x) * (this.x - x) + (this.y - y) * (this.y - y));
+    }
+
+    public double distance (Point a) {
+        return distance(a.x, a.y);
+    }
+}
