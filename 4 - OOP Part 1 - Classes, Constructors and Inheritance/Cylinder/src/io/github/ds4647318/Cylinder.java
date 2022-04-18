66aa5bb1c0d7c02595de8f5e7581fe2fa0fca90f
Mubarek Seid <mubareksd@gmail.com>
2022-04-18 18:08:20 +0300

    Coding Exercise 35: Cylinder

---
+++ 4 - OOP Part 1 - Classes, Constructors and Inheritance/Cylinder/src/io/github/ds4647318/Cylinder.java
@@ -0,0 +1,24 @@
+package io.github.ds4647318;
+
+public class Cylinder extends Circle {
+
+    private double height;
+
+    public Cylinder(double radius, double height) {
+        super(radius);
+
+        if (height < 0) {
+            height = 0;
+        }
+        this.height = height;
+    }
+
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        return (height * getArea());
+    }
+}
