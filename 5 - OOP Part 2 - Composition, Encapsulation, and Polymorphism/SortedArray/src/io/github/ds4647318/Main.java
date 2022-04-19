28769fdb94ffd405f1b7d9cce8257a4343268271
Mubarek Seid <mubareksd@gmail.com>
2022-04-19 19:27:25 +0300

    Coding Exercise 41: Sorted Array

---
+++ 5 - OOP Part 2 - Composition, Encapsulation, and Polymorphism/SortedArray/src/io/github/ds4647318/Main.java
@@ -0,0 +1,14 @@
+package io.github.ds4647318;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int[] myIntegers = SortedArray.getIntegers(5);
+        int[] sorted = SortedArray.sortIntegers(myIntegers);
+        SortedArray.printArray(sorted);
+
+    }
+
+}
