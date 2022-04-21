1ee21e16fe9f84cd459aa10df135a5f8b381d754
Mubarek Seid <mubareksd@gmail.com>
2022-04-21 20:44:41 +0300

    105. List And ArrayList Part 2

--- 6 - Arrays, Java inbuilt Lists, Autoboxing and unboxing/ListAndArrayList/src/io/github/ds4647318/GroceryList.java
+++ 6 - Arrays, Java inbuilt Lists, Autoboxing and unboxing/ListAndArrayList/src/io/github/ds4647318/GroceryList.java
@@ -26,8 +26,15 @@ public class GroceryList {
     public void removeGroceryItem(int position) {
         String theItem = groceryList.get(position);
         groceryList.remove(position);
-        // Milk
-        // Bread
+    }
+
+    public String findItem(String searchItem) {
+//        boolean exists = groceryList.contains(searchItem);
+        int position = groceryList.indexOf(searchItem);
+        if (position >= 0) {
+            return groceryList.get(position);
+        }
+        return null;
     }
 
 }
